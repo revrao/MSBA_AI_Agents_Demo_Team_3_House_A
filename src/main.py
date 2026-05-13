5ebf0218ba1db5d574d1e4fe6fa667e85a05a3f1
matheus-kina <matheus.yukio.kina.2026@anderson.ucla.edu>
2026-05-13 16:55:08 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
 from __future__ import annotations
 from dotenv import load_dotenv
-load_dotenv()  # must be before importing graph/agents
+load_dotenv()
 from tracing import init_langsmith_tracing
-init_langsmith_tracing()  # must be before importing graph/agents
+init_langsmith_tracing()
 from graph import build_graph
-
 
 if __name__ == "__main__":
  
     app = build_graph()
 
+    # Define the scenario you want to test
+    scenario = "A severe driver shortage occurred in corridor A, resulting in 30% fewer available standard trucks. At the same time, we experienced a 20% demand spike."
+
     state = {
         "pdf_path": "data/SeeWeeS Specialty distribution.pdf",
         "csv_path": "data/Incoming_shipment_02_08.csv",
+        "what_if_scenario": scenario, # NEW: Inject scenario here
     }
 
     final = app.invoke(state)
